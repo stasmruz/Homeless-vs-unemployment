458127d73caab8ecbd460972c277fffbdd23da08
Barlow0471 <mattbar2@comcast.net>
2021-09-08 20:59:23 -0400

    Updated physical schema and database connection

--- PhysicalSchema.sql
+++ PhysicalSchema.sql
@@ -1,12 +1,13 @@
-CREATE TABLE Homelessness_Unemployment (
-	State_id VARCHAR NOT NULL,
+CREATE TABLE homelessnessdb (
+	State_id INT NOT NULL,
 	State VARCHAR NOT NULL,
 	Unemployment_Rate REAL NOT NULL,
-	Under_18 VARCHAR NOT NULL,
-	Age_18_to_24 VARCHAR NOT NULL,
-	Age_24_and_over VARCHAR NOT NULL,
-	Female VARCHAR NOT NULL,
-	Male VARCHAR NOT NULL,
-	Transgender VARCHAR NOT NULL,
-	Overall_Homeless VARCHAR NOT NULL
+	Unemployment_Rank INT NOT NULL,
+	Under_18 INT NOT NULL,
+	Age_18_to_24 INT NOT NULL,
+	Age_24_and_over INT NOT NULL,
+	Female INT NOT NULL,
+	Male INT NOT NULL,
+	Transgender INT NOT NULL,
+	Overall_Homeless INT NOT NULL
 );
